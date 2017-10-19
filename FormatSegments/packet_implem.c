3a46b23ff63dff64f7579aaf9ee2ff77f9169697
fblondiaux <florence.blondiaux@student.uclouvain.be>
2017-10-19 09:44:43 +0200

    Modifications crc

--- FormatSegments/packet_implem.c
+++ FormatSegments/packet_implem.c
@@ -1,5 +1,5 @@
-			#include "packet_interface.h"
-# include <zlib.h> /* crc32 */
+#include "packet_interface.h"
+#include <zlib.h> /* crc32 */
 #include <stdlib.h> /* malloc/calloc */
 #include <string.h> /* memcpy */
 #include <arpa/inet.h> /* htons */
@@ -45,100 +45,103 @@ pkt_t* pkt_new()
 
 void pkt_del(pkt_t *pkt)
 {
-    if(pkt->payload != NULL)
-    	free(pkt->payload);
+	if(pkt->payload != NULL)
+	free(pkt->payload);
 
-    free(pkt);
+	free(pkt);
 }
 
-pkt_status_code pkt_decode(const char *data, const size_t len, pkt_t *pkt)
-{
-	// packet recu en network byte order ! ->est ce qu'il faut mettre des ntohs un peu partout?
-
-	size_t err = 0;
-	pkt_status_code ret = 0;
-
-	memcpy(pkt, data, 1);
-
-	if(pkt->type!=1 && pkt->type!=2 && pkt->type!=3) //verifie validite type packet
-		return E_TYPE;
-
-	uint8_t seqnum = 0;
-	uint16_t length = 0;
-	uint32_t timestamp = 0;
-	uint32_t crc1 = 0;
-	uint32_t crc2 = 0;
-
-	memcpy(&seqnum, data, 1);
-	ret = pkt_set_seqnum(pkt, seqnum);
-	if(ret!= PKT_OK)
-		return E_UNCONSISTENT;
-
-	memcpy(&length, data, 2);
-	ret = pkt_set_length(pkt, length);
-	if(ret != PKT_OK)
-		return E_UNCONSISTENT;
-
-	memcpy(&timestamp, data, 4);
-	ret = pkt_set_timestamp(pkt, timestamp);
-	if(ret!= PKT_OK)
-		return E_UNCONSISTENT;
-
-	// pas fini
-
-
-
-
-}
+// pkt_status_code pkt_decode(const char *data, const size_t len, pkt_t *pkt)
+// {
+// 	// packet recu en network byte order ! ->est ce qu'il faut mettre des ntohs un peu partout?
+//
+// 	size_t err = 0;
+// 	pkt_status_code ret = 0;
+//
+// 	memcpy(pkt, data, 1);
+//
+// 	if(pkt->type!=1 && pkt->type!=2 && pkt->type!=3) //verifie validite type packet
+// 		return E_TYPE;
+//
+// 	uint8_t seqnum = 0;
+// 	uint16_t length = 0;
+// 	uint32_t timestamp = 0;
+// 	uint32_t crc1 = 0;
+// 	uint32_t crc2 = 0;
+//
+// 	memcpy(&seqnum, data, 1);
+// 	ret = pkt_set_seqnum(pkt, seqnum);
+// 	if(ret!= PKT_OK)
+// 		return E_UNCONSISTENT;
+//
+// 	memcpy(&length, data, 2);
+// 	ret = pkt_set_length(pkt, length);
+// 	if(ret != PKT_OK)
+// 		return E_UNCONSISTENT;
+//
+// 	memcpy(&timestamp, data, 4);
+// 	ret = pkt_set_timestamp(pkt, timestamp);
+// 	if(ret!= PKT_OK)
+// 		return E_UNCONSISTENT;
+//
+//}
 
 pkt_status_code pkt_encode(const pkt_t* pkt, char *buf, size_t *len)
 {
 	int count = 0;
-	// Verifier aussi si payload present.
-	if( *len < 12) // verifie qu'il y a assez de place dans buffer pour mettre tout jusqu'au crc1
+	if( buf==NULL || *len == 0)
+	return E_NOMEM;
+
+	if( pkt == NULL)
+	return E_UNCONSISTENT;
+
+	if(*len < 12)
+	return E_NOMEM;
+
+	if(pkt->length != 0 && *len < (pkt->length)+4+12 ) // si payload du pkt non nul, il faut verifier qu'il y a assez de place dans buf
 	{
 		return E_NOMEM;
 	}
-	// On mettrait tr direct à 0 ?
-	memcpy(buf, pkt, sizeof(uint8_t)); // copie de window,tr,type
-	memcpy(buf+1, pkt->seqnum, sizeof(uint8_t)); // copie seqnum
-	count = count + 2;
-	uint16_t length = htons(pkt_get_length(pkt));
-	memcpy(buf+count, &length, sizeof(uint16_t)); // copie de lentgh
-	count = count+2;
-	memcpy(buf+count, pkt->timestamp, sizeof(uint32_t)); // sans endianness particuliere, htons ?
-	count = count+4;
 
-	// F= Ça ne compile pas car pkt est const. En soi c'est logique mais comme tr
-	// Est un des premiers trucs a mettre dans le buffer, on le met quand à 0
-	pkt_set_tr(pkt, 0); //Vérifier la valeur de retour ?
-		// De ce que j'ai compris il calcule un crc sur les donnees passées dans le
-		// Deuxiemme argument.
-				//http://netbsd.gw.com/cgi-bin/man-cgi?zlib+3+NetBSD-current
-				//uLong crc32(uLong crc, const Bytef *buf, uInt len);
-				//The crc32() function updates a running CRC with the bytes
-				//buf[0..len-1] and returns the updated CRC.  If buf is NULL, this
-				//function returns the required initial value for the CRC.  Pre-
-				//and post-conditioning (one's complement) is performed within this
-				//function so it shouldn't be done by the application.
+	///
+	memcpy(buf, pkt, 8); // copie de window,tr,type, length, timestamp
+	pkt_t* temp = (pkt_t *) malloc(8);
+	if(temp == NULL)
+	return E_NOMEM;
+	memcpy(temp, pkt, 8);
+	temp->trFlag = 0;
+	count = 8;
+	char* buftemp = (char *) malloc(8);
+	if(buftemp == NULL)
+	return E_NOMEM;
 
+	memcpy(buftemp, temp ,8); // copie de window,tr=0,type, seqnum, length, timestamp
 
-		// Ce qu'on m'a dit c'est crééer une structure identique sauf pour tr qu'on 
-		// met a zero , on calcule le crc la dessus.
-	uLong crc1 = crc32(pkt_get_crc1(pkt),(Bytef*) buf, count);
+	uLong crc = crc32(0L, Z_NULL, 0);
+	uLong crc1 = crc32(crc,(Bytef*) temp, count);
 
- /// Mettre crc1 ds buffer
- // SI payload présent.
- 	// F: pour la suite je dirais qu'il faut mettre le payload ( de nouveau comment)
-	// sEt quand payload est ajouté
-	// Alors on fait crc
+	memcpy(buf+count, &crc1, 4);
+	count +=4;
+
+	if (pkt_get_length(pkt) != 0) {
+		memcpy(buf+count,pkt_get_payload(pkt),pkt_get_length(pkt));
+		count += pkt_get_length(pkt);
+
+		uLong crc2 = crc32(crc, (Bytef*) pkt_get_payload(pkt), pkt_get_length(pkt));
+
+		memcpy(buf+count, &crc2, 4);
+		count += 4;
+	}
+	*len = count;
+	free(temp);
+	free(buftemp);
+
+	return PKT_OK;
 }
-
-
 /* Accesseurs pour les champs toujours presents du paquet.
- * Les valeurs renvoyees sont toutes dans l'endianness native
- * de la machine!
- */
+* Les valeurs renvoyees sont toutes dans l'endianness native
+* de la machine!
+*/
 ptypes_t pkt_get_type  (const pkt_t* pkt)
 {
 	return pkt->type;
@@ -174,8 +177,8 @@ uint32_t pkt_get_crc1   (const pkt_t* pkt)
 	return ntohl(pkt->crc1);
 }
 /* Renvoie le CRC2 dans l'endianness native de la machine. Si
- * ce field n'est pas present, retourne 0.
- */
+* ce field n'est pas present, retourne 0.
+*/
 uint32_t pkt_get_crc2   (const pkt_t* pkt)
 {
 	if(pkt_get_tr(pkt) != 0 || pkt_get_length(pkt) == 0){
@@ -185,8 +188,8 @@ uint32_t pkt_get_crc2   (const pkt_t* pkt)
 }
 
 /* Renvoie un pointeur vers le payload du paquet, ou NULL s'il n'y
- * en a pas.
- */
+* en a pas.
+*/
 const char* pkt_get_payload(const pkt_t* pkt)
 {
 	if(pkt_get_length(pkt) == 0){
@@ -196,10 +199,10 @@ const char* pkt_get_payload(const pkt_t* pkt)
 }
 
 /* Setters pour les champs obligatoires du paquet. Si les valeurs
- * fournies ne sont pas dans les limites acceptables, les fonctions
- * doivent renvoyer un code d'erreur adapte.
- * Les valeurs fournies sont dans l'endianness native de la machine!
- */
+* fournies ne sont pas dans les limites acceptables, les fonctions
+* doivent renvoyer un code d'erreur adapte.
+* Les valeurs fournies sont dans l'endianness native de la machine!
+*/
 
 pkt_status_code pkt_set_type(pkt_t *pkt, const ptypes_t type)
 {
@@ -210,7 +213,7 @@ pkt_status_code pkt_set_type(pkt_t *pkt, const ptypes_t type)
 		pkt->type = type;
 	}
 	else
-		return E_TYPE;
+	return E_TYPE;
 
 	return PKT_OK;
 }
@@ -238,7 +241,7 @@ pkt_status_code pkt_set_window(pkt_t *pkt, const uint8_t window)
 pkt_status_code pkt_set_seqnum(pkt_t *pkt, const uint8_t seqnum)
 {
 	if( seqnum >= 255)
-		return E_SEQNUM;
+	return E_SEQNUM;
 
 	if(pkt->type==PTYPE_DATA || pkt->type==PTYPE_NACK || pkt->type==PTYPE_ACK)
 	{
@@ -252,7 +255,7 @@ pkt_status_code pkt_set_seqnum(pkt_t *pkt, const uint8_t seqnum)
 pkt_status_code pkt_set_length(pkt_t *pkt, const uint16_t length)
 {
 	if(length >= 512)
-		return E_LENGTH;
+	return E_LENGTH;
 
 	pkt->length = htons(length);
 	return PKT_OK;
@@ -282,16 +285,16 @@ pkt_status_code pkt_set_crc2(pkt_t *pkt, const uint32_t crc2)
 }
 
 /* Defini la valeur du champs payload du paquet.
- * @data: Une succession d'octets representants le payload
- * @length: Le nombre d'octets composant le payload
- * @POST: pkt_get_length(pkt) == length */
+* @data: Une succession d'octets representants le payload
+* @length: Le nombre d'octets composant le payload
+* @POST: pkt_get_length(pkt) == length */
 pkt_status_code pkt_set_payload(pkt_t *pkt, const char *data, const uint16_t length)
 {
 	//length &= 0xFFFF;
 	// question : length en host byte order??
 
 	if(pkt == NULL)
-		return E_UNCONSISTENT; // packet incohérent
+	return E_UNCONSISTENT; // packet incohérent
 
 	if(pkt->payload != NULL) // il y avait deja un packet
 	{
@@ -299,7 +302,7 @@ pkt_status_code pkt_set_payload(pkt_t *pkt, const char *data, const uint16_t len
 		pkt->payload = NULL;
 		pkt_status_code err = pkt_set_length(pkt, htons(0));
 		if(err != PKT_OK)
-			return E_LENGTH;
+		return E_LENGTH;
 	}
 
 	if(data == NULL || length == 0) // payload nul
@@ -312,12 +315,12 @@ pkt_status_code pkt_set_payload(pkt_t *pkt, const char *data, const uint16_t len
 		{
 			pkt->payload = (char *)malloc(length*sizeof(char));
 			if(pkt->payload == NULL)
-				return E_NOMEM; // a verifier
+			return E_NOMEM; // a verifier
 
 			memcpy((void *)pkt->payload, (void *) data, length);
 			pkt_status_code err = pkt_set_length(pkt, htons(length));
 			if(err != PKT_OK)
-				return E_LENGTH;
+			return E_LENGTH;
 
 			return PKT_OK;
 		}
