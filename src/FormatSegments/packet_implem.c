16bcce4b66466229106978b2d56f40c89fae12dd
NoemieVst <noemie.verstraete@student.uclouvain.be>
2017-10-26 12:57:55 +0200

    essai

--- src/FormatSegments/packet_implem.c
+++ src/FormatSegments/packet_implem.c
@@ -91,18 +91,28 @@ pkt_status_code pkt_decode(const char *data, const size_t len, pkt_t *pkt)
 			return E_UNCONSISTENT;
 		}
 	}
-	memcpy(pkt, data, 12); // copie des 12 prem bytes (jusqu'a crc1 y compris)
+
+	memcpy(pkt, data, 12); // copie des 12 prem bytes (en network BO) (jusqu'a crc1 y compris)
 
 	// Préparation pour la vérification de crc1
 	if(pkt_get_tr(pkt) == 1 )
 	{
-
 		return E_TR;
 	}
+	// mettre length en host byte order dans pkt
+	if(pkt_set_length(pkt,ntohs(pkt_get_length(pkt))) != PKT_OK){ // On met length en host byte order
+		return err;
+	}
+	if(pkt_set_crc1(pkt,ntohl(pkt_get_crc1(pkt))) != PKT_OK){ // On met le crc1 en host byte order
+		return err;
+	}
 
-
-	if(crc32(crc,(Bytef*) data, 8) != pkt_get_crc1(pkt) )
+	fprintf(stderr, "decode : crc1 mis dans packet (en host): %u\n", pkt_get_crc1(pkt) );
+	fprintf(stderr, "decode : calcule crc1 (des donnees en host): %lu\n",crc32(crc,(Bytef*) data, 8 ) );
+	
+	if(crc32(crc,(Bytef*) data, 8 ) != pkt_get_crc1(pkt) )
 	{
+		fprintf(stderr, "Le crc1 calculé n'est pas le meme que celui qui a été reçu\n");
 		return E_CRC;
 	}
 	// Verification length
@@ -115,18 +125,18 @@ pkt_status_code pkt_decode(const char *data, const size_t len, pkt_t *pkt)
 	}
 
 	if(taille_payl != 0){
-		char* payload = (char *) malloc( taille_payl);
+		char* payload = (char *) malloc(taille_payl);
 		if(payload == NULL) {
 			return E_NOMEM;
 		}
 		memcpy(payload, data+12, taille_payl); // copie du payload dans payload
-		uint32_t crc2 ;
+		uint32_t crc2;
 		memcpy(&crc2, data+12+taille_payl, 4);
 		crc2 = ntohl(crc2);
 
 
 		if( crc32(crc, (Bytef *) data+12, taille_payl) != crc2){ // verifie que le payload a la bonne valeur du crc2
-
+			fprintf(stderr, "Attention, le crc2 n'est pas le même que ce que l'on calcule\n" );
 			return E_CRC;
 		}
 		err = pkt_set_payload(pkt,data+12, taille_payl);
@@ -143,7 +153,7 @@ pkt_status_code pkt_decode(const char *data, const size_t len, pkt_t *pkt)
 }
 
 
-
+// struct pkt -> buffer
 pkt_status_code pkt_encode(const pkt_t* pkt, char *buf, size_t *len)
 {
 	int count = 0;
@@ -155,10 +165,8 @@ pkt_status_code pkt_encode(const pkt_t* pkt, char *buf, size_t *len)
 		return PKT_OK;
 
 	if(*len < 12){
-
 			return E_NOMEM;
 	}
-
 
 	if(pkt_get_length(pkt) != 0 && *(len) < (size_t) (pkt_get_length(pkt))+4+12 ) // si payload du pkt non nul, il faut verifier qu'il y a assez de place dans buf
 	{
@@ -166,42 +174,100 @@ pkt_status_code pkt_encode(const pkt_t* pkt, char *buf, size_t *len)
 	}
 
 
-	///
-	memcpy(buf, pkt, 8); // copie de window,tr,type, length, timestamp
-	pkt_t* temp = (pkt_t *) malloc(8);
-	if(temp == NULL){
+	//// chmgt
 
+	memcpy(buf, pkt, 2); // memcpy de wind, ty, type et seqnum
+	count = count+2;
+	uint16_t l =  htons(pkt_get_length(pkt));
+	memcpy(buf+count,& l, 2); // copie de length en network byte order
+	count = count +2;
+	//copie de timestamp
+	uint32_t t = pkt_get_timestamp(pkt);
+	memcpy(buf+count,&t, 4);
+	count = count +4;
 
-			return E_NOMEM;
-	}
+	pkt_t *temp = (pkt_t *)malloc(sizeof(pkt_t));
+	// copie de char* buf à pkt_t temp pour mettre tr à 0
+	memcpy(temp, buf, 8);
+	pkt_set_tr(temp, 0);
 
-	memcpy(temp, pkt, 8);
-	temp->trFlag = 0;
-	count = 8;
 	char buftemp[8];
-
-	memcpy(buftemp, temp ,8); // copie de window,tr=0,type, seqnum, length, timestamp
+	// copie du packet avec tr=0 dans un char*
+	memcpy(buftemp, temp, 8);
 
 	uLong crc = crc32(0L, Z_NULL, 0);
+	//calcul du crc1 du packet en network
 	uLong crc1 = crc32(crc,(Bytef*) buftemp, count);
-	crc1 = htonl(crc1);
-	memcpy(buf+count, &crc1, 4);
-	count +=4;
 
-	if (pkt_get_length(pkt) != 0) {
-		memcpy(buf+count,pkt_get_payload(pkt),pkt_get_length(pkt));
-		count += pkt_get_length(pkt);
+	//fprintf(stderr, "Encoder : crc1 qu'il y avait dans packet: %u\n", pkt_get_crc1(pkt));
+	fprintf(stderr, "Encoder : calcul crc1 en host: %lu\n",crc32(crc,(Bytef*) buftemp, count ) );
+	// crc1 [host] --> crc1[network]
+	crc1 = htonl(crc1);
+	fprintf(stderr, "Encoder : conversion crc1 en network (pour le mettre dans buffer): %lu\n",crc1);
+
+	// mise crc1 en network byte order dans pkt
+	memcpy(buf+count, &crc1, 4);
+	count = count+4;
+	if (pkt_get_length(pkt) != 0)
+	{
+		memcpy(buf+count,  pkt_get_payload(pkt), pkt_get_length(pkt));
+		count = count+pkt_get_length(pkt);
 
 		uLong crc2 = crc32(crc, (Bytef*) pkt_get_payload(pkt), pkt_get_length(pkt));
 		crc2 = htonl(crc2);
 		memcpy(buf+count, &crc2, 4);
-		count += 4;
+		count = count+4;
 	}
 	*len = count;
 	free(temp);
-
 	return PKT_OK;
 }
+
+
+
+
+	////
+// 	memcpy(buf, pkt, 8); // copie de window,tr,type, length, timestamp
+// 	pkt_t* temp = (pkt_t *) malloc(8);
+// 	if(temp == NULL){
+//
+//
+// 			return E_NOMEM;
+// 	}
+//
+// 	memcpy(temp, pkt, 8);
+// 	temp->trFlag = 0;
+// 	count = 8;
+// 	char buftemp[8];
+//
+// 	memcpy(buftemp, temp ,8); // copie de window,tr=0,type, seqnum, length, timestamp
+//
+// 	uLong crc = crc32(0L, Z_NULL, 0);
+// 	uLong crc1 = crc32(crc,(Bytef*) buftemp, count);
+// 	fprintf(stderr, "pkt_encode : \n");
+// 	fprintf(stderr, "crc1 dans packet : %lu\n", crc1 );
+// 	crc1 = htonl(crc1);
+//
+//
+// 	memcpy(buf+count, &crc1, 4);
+// 	count +=4;
+//
+// 	if (pkt_get_length(pkt) != 0) {
+// 		memcpy(buf+count,pkt_get_payload(pkt),pkt_get_length(pkt));
+// 		count += pkt_get_length(pkt);
+//
+// 		uLong crc2 = crc32(crc, (Bytef*) pkt_get_payload(pkt), pkt_get_length(pkt));
+// 		crc2 = htonl(crc2);
+// 		memcpy(buf+count, &crc2, 4);
+// 		count += 4;
+// 	}
+// 	*len = count;
+// 	free(temp);
+//
+// 	return PKT_OK;
+// }
+
+
 /* Accesseurs pour les champs toujours presents du paquet.
 * Les valeurs renvoyees sont toutes dans l'endianness native
 * de la machine!
@@ -228,7 +294,7 @@ uint8_t  pkt_get_seqnum(const pkt_t* pkt)
 
 uint16_t pkt_get_length(const pkt_t* pkt)
 {
-	return ntohs(pkt->length);
+	return pkt->length; //chgmt
 }
 
 uint32_t pkt_get_timestamp   (const pkt_t* pkt)
@@ -238,7 +304,7 @@ uint32_t pkt_get_timestamp   (const pkt_t* pkt)
 
 uint32_t pkt_get_crc1   (const pkt_t* pkt)
 {
-	return ntohl(pkt->crc1);
+	return pkt->crc1; // chgmt
 }
 /* Renvoie le CRC2 dans l'endianness native de la machine. Si
 * ce field n'est pas present, retourne 0.
@@ -248,7 +314,7 @@ uint32_t pkt_get_crc2   (const pkt_t* pkt)
 	if(pkt_get_tr(pkt) != 0 || pkt_get_length(pkt) == 0){
 		return 0;
 	}
-	return ntohl(pkt->crc2);
+	return pkt->crc2; // chgmt
 }
 
 /* Renvoie un pointeur vers le payload du paquet, ou NULL s'il n'y
@@ -321,7 +387,7 @@ pkt_status_code pkt_set_length(pkt_t *pkt, const uint16_t length)
 	if(length > 512)
 	return E_LENGTH;
 
-	pkt->length = htons(length);
+	pkt->length = length; // chgmt
 	return PKT_OK;
 }
 
@@ -333,7 +399,7 @@ pkt_status_code pkt_set_timestamp(pkt_t *pkt, const uint32_t timestamp)
 
 pkt_status_code pkt_set_crc1(pkt_t *pkt, const uint32_t crc1)
 {
-	pkt->crc1 = htonl(crc1);
+	pkt->crc1 = crc1;
 	return PKT_OK;
 }
 
@@ -341,7 +407,7 @@ pkt_status_code pkt_set_crc2(pkt_t *pkt, const uint32_t crc2)
 {
 	if(pkt->payload != NULL && pkt->trFlag == 0)
 	{
-		pkt->crc2 = htonl(crc2);
+		pkt->crc2 = crc2; // chgmt
 		return PKT_OK;
 	}
 	return E_UNCONSISTENT;
