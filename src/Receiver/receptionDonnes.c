81a1e0a6b8068921ae278e97174bde0967e5e371
Noemie Verstraete <verstraeteno@amidala.info.ucl.ac.be>
2017-11-03 16:05:28 +0100

    :( :(

--- src/Receiver/receptionDonnes.c
+++ src/Receiver/receptionDonnes.c
@@ -54,6 +54,7 @@ selectiveRepeat_status_code traitementRecu(char* buf, int taille, char* ACK, siz
 
   if (err == E_TR && pkt_get_type(reception) == 1){
     // On va devoir envoyer un NACK
+    fprintf(stderr, "Receiver doit renvoyer un nack\n");
     pkt_t* ack = pkt_new(); // Préparation de la structure à encoder
     pkt_set_type(ack, 3);
     pkt_set_window(ack, window);
@@ -70,78 +71,74 @@ selectiveRepeat_status_code traitementRecu(char* buf, int taille, char* ACK, siz
     pkt_del(reception);
     pkt_del(ack);
     return S_NACK;
-
+    // N : comment le nack est-il envoyé?
   }
-
   else if(err == PKT_OK){
     // On doit envoyer un ACK et écrire ce qu'il y avait dans le payload.
     int seqnum = pkt_get_seqnum(reception);
     if(seqnumMin < seqnumMax){
       if (seqnum < seqnumMin || seqnum > seqnumMax){ // En dehors de ce que l'on peux recevoir.
-      pkt_del(reception);
-      fprintf(stderr, "J'ai déja reçu le sequnum %d, j'ignore\n", seqnum );
-      return INGNORE;
+        pkt_del(reception);
+        fprintf(stderr, "Sender a déja reçu le sequnum %d, il l'ignore\n", seqnum );
+        return INGNORE;
       }
     }
     if(seqnumMax < seqnumMin){
       if (seqnum > seqnumMax && seqnum < seqnumMin){ // En dehors de ce que l'on peux recevoir.
-      pkt_del(reception);
-      fprintf(stderr, "J'ai déja reçu le sequnum %d, j'ignore\n", seqnum );
-      return INGNORE;
+        pkt_del(reception);
+        fprintf(stderr, "Sender a déja reçu le sequnum %d, il l'ignore\n", seqnum );
+        return INGNORE;
       }
     }
+    // Creation et placement de la structure dans le buffer.
+    struct buffer* new = malloc(sizeof(struct buffer));
+    new->seqnum = seqnum;
+    new->data = reception;
+    insertStruct(new);
+    window--;
+    //Envoie du ack.
+    struct buffer* current = startBuffer;
+    char payloadTemp[512];
+    int size = 0;
+    while(startBuffer != NULL && startBuffer->seqnum == seqnumMin){
+      // Decalage de la fenêtre
+      seqnumMin = (seqnumMin +1) % 256;
+      seqnumMax = (seqnumMax +1)%256;
 
+      //fprintf(stderr, "Mes numeros de sequnums, mis à jours sont min %d, max %d\n", seqnumMin , seqnumMax );
 
-  // Creation et placement de la structure dans le buffer.
-  struct buffer* new = malloc(sizeof(struct buffer));
-  new->seqnum = seqnum;
-  new->data = reception;
-  insertStruct(new);
-  window--;
-  //Envoie du ack.
-  struct buffer* current = startBuffer;
-  char payloadTemp[512];
-  int size = 0;
-  while(startBuffer != NULL && startBuffer->seqnum == seqnumMin){
-    // Decalage de la fenêtre
-    seqnumMin = (seqnumMin +1) % 256;
-    seqnumMax = (seqnumMax +1)%256;
+      lasttimestamp = pkt_get_timestamp(startBuffer->data); // ON en a besoin pour la suite
 
-    //fprintf(stderr, "Mes numeros de sequnums, mis à jours sont min %d, max %d\n", seqnumMin , seqnumMax );
-
-    lasttimestamp = pkt_get_timestamp(startBuffer->data); // ON en a besoin pour la suite
-
-    size = pkt_get_length(current->data);
-    memcpy( payloadTemp,pkt_get_payload(current->data), size);
-    int ecrit = write(file, payloadTemp, size);
-    if(ecrit!=size){
-      fprintf(stderr, "Le segment à été reçu mais un problème à été rencontré lors de l'écriture de son payload\n");
+      size = pkt_get_length(current->data);
+      memcpy( payloadTemp,pkt_get_payload(current->data), size);
+      int ecrit = write(file, payloadTemp, size);
+      if(ecrit!=size){
+        fprintf(stderr, "Le segment à été reçu mais un problème à été rencontré lors de l'écriture de son payload\n");
+      }
+      startBuffer = startBuffer->next;
+      pkt_del(current->data);
+      free(current);
+      window ++; // ON a libere une place dans le buffer
     }
-    startBuffer = startBuffer->next;
-    pkt_del(current->data);
-    free(current);
-    window ++; // ON a libere une place dans le buffer
-  }
-  pkt_t* ack = pkt_new();
-  pkt_set_type(ack, 2);
-  pkt_set_window(ack, window);
-  pkt_set_seqnum(ack,seqnumMin);
-  pkt_set_length(ack,0);
-  pkt_set_timestamp(ack,lasttimestamp);
-  if(pkt_encode(ack,ACK, (size_t*) SizeACK) != PKT_OK){
+    pkt_t* ack = pkt_new();
+    pkt_set_type(ack, 2);
+    pkt_set_window(ack, window);
+    pkt_set_seqnum(ack,seqnumMin);
+    pkt_set_length(ack,0);
+    pkt_set_timestamp(ack,lasttimestamp);
+    if(pkt_encode(ack,ACK, (size_t*) SizeACK) != PKT_OK){
+      pkt_del(ack);
+      return INGNORE;
+    }
 
     pkt_del(ack);
+    return S_ACK;
+
+  }
+  else{
+    pkt_del(reception);
     return INGNORE;
   }
-
-  pkt_del(ack);
-  return S_ACK;
-
-}
-else{
-  pkt_del(reception);
-  return INGNORE;
-}
 }
 
 
@@ -177,15 +174,17 @@ void receptionDonnes(int sfd, int file){
       err = INGNORE;
       // traite la lecture
       if (ufds[0].revents & POLLIN) {
+        fprintf(stderr, "il y a de quoi lire\n");
         memset((void*)payload, 0, 512); // make sure the struct is empty
         payloadSize = 512;
         int recu = read(sfd, buf, sizeof buf); // receive normal data
         if(recu == 12){ //faire un truc + propre
+          fprintf(stderr, "J'ai reçu le dernier packer, 0\n");
           end = 1;
         }
         else{
-        err = traitementRecu(buf, recu, payload, &payloadSize, file);
-        //fprintf(stderr, "Mes numeros de sequnums après l'envoie du ack sont min %d, max %d\n", seqnumMin , seqnumMax );
+          err = traitementRecu(buf, recu, payload, &payloadSize, file);
+          fprintf(stderr, "Mes numeros de sequnums après l'envoie du ack sont min %d, max %d\n", seqnumMin , seqnumMax );
         }
       }
       if(err != INGNORE && ufds[1].revents & POLLOUT){
@@ -194,7 +193,6 @@ void receptionDonnes(int sfd, int file){
           fprintf(stderr, "Erreur lors de l'envoi de l'acquitement\n");
         }
       }
-
     }
 
   }
